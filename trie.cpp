6ea027c481ff1a060ce77058e6bddddf79578859
rzillo <42211951+rzillo@users.noreply.github.com>
2020-05-08 18:05:53 -0300

    criação trie.cpp

---
+++ trie.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector> 
+#include <string>
+#include <bits/stdc++.h>
+#include <chrono>
+
+using namespace std;
+const int ALPHABET_SIZE = 26; 
+
+class Node {
+public:
+  Node* pChilds[26];
+}
