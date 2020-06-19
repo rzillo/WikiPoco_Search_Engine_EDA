2beab5f203265cc3e9b3196b0444bad58eb8eedc
renanfmoura <42002454+renanfmoura@users.noreply.github.com>
2020-06-18 22:26:50 -0300

    Last commit serialization

--- serialization.cpp
+++ serialization.cpp
@@ -1,112 +1,58 @@
 #include <iostream>
+#include <vector>
 #include <sstream>
 #include <fstream>
-#include <map>
+#include <chrono>
+#include <string>
+#include "code.hpp"
 
-struct Node{
-    public:
-    string name; 
-    bool end;    
-    map<string, Node *> child; 
-    Node(string n): name(n), end(0){   }
-    Node(): end(0){ } 
-};
-class Tree{
-    private:
-    Node * pRoot;    
-    public:    
-    Tree(){
-        pRoot = new Node();
-    }
 
-    void insert(string full_name){
-        stringstream split; 
-        split << full_name; 
-        string cur_name;
-        Node ** pNode = &pRoot; 
-        Node * pNew; 
-      
-        while(split >> cur_name){
-            if(!(*pNode)->child[cur_name]){ 
-                pNew = new Node(cur_name); 
-                (*pNode)->child[cur_name] = pNew;  
-            }
-            pNode = &((*pNode)->child[cur_name]); 
-        }
-    }
+using namespace std;
 
-    void search(string full_name){
-        cout << "Procurando por: " << full_name << endl;
-        stringstream split;
-        split << full_name; 
-        string cur_name;
-        Node * pNode = pRoot; 
-        while(split >> cur_name){
-            if(pNode->child[cur_name]){ 
-                pNode = pNode->child[cur_name];
-            }else{
-                cout << "Nome nao encontrado." << endl; 
-                return;
-            }
-        }
-        cout << "Nome encontrado." << endl; 
-        return;
+int main () {
+  
+  Trie teste;
+  ifstream names("files");
+  ifstream file;
+  string s;
+  string s1;
+  string word;
+  int index;
+  int docs = 0;
+  
+  auto t0 = chrono::high_resolution_clock::now();
+  while (getline(names, s)) {
+    file.open("C:/Users/renan/Desktop/final_test/wiki_files/" + s +"(conv)");
+    cout << "lendo arquivo " << s << "\n";
+    while (getline(file, s1)) {
+      if (s1.substr(0,4) == "<doc") {
+	docs++;
+	index = get_number(s1);
+      } else {
+	if (s1.substr(0,5) != "</doc" && s1.substr(0,5) != "ENDOF"){
+	  for (string::iterator it = s1.begin(); it != s1.end(); ++it) {
+	    if (*it == ' ' ||*it == '-' ||*it == '.'||*it == ',' || *it == ';'|| *it == ':'||*it == '?'|| *it == '!') {	      
+	      teste.insert(word, index);
+	      word.clear();
+	    } else {
+	      word += *it;
+	    }
+	  }
+	}
+      }
     }
-
-    void serialize(string file_name){ 
-        ofstream file;
-        file.open(file_name);
-        Node * pNode = pRoot;
-        run_serialize(pNode, file); 
-    }
-
-    void run_serialize(Node * pCur, ofstream & file){
-        file << pCur->name << " "; 
-        for(auto const& item : pCur->child){             
-            cout << item.first << endl;
-            run_serialize(item.second, file); 
-        }
-        
-        file << ") "; 
-    }
-    
-    void diserialize(string file_name){
-        ifstream file; 
-        string line; 
-        file.open(file_name); 
-        getline(file, line);
-        Node ** pNode = &pRoot; 
-        stringstream split; 
-        split << line; 
-        run_pRoot_diserialize(pNode, split);
-    }
-
-    void run_pRoot_diserialize(Node ** pNode, stringstream & split){
-        string cur_name; 
-        while(split >> cur_name){
-        
-            if(run_diserialize(pNode, cur_name, split)) break;
-        }
-    }
-
-    bool run_diserialize(Node ** pNode, string cur_name, stringstream  & split){
-        if(cur_name == ")") return 1;
-        Node * pNew = new Node(cur_name); 
-        (*pNode)->child[cur_name] = pNew; 
-        pNode = &(*pNode)->child[cur_name]; 
-        while(split >> cur_name){ 
-            if(run_diserialize(pNode, cur_name, split)) break;
-        }
-        return 0;
-    }
-
-};
-int main(){
-    Tree nomes;
-    //nomes.insert("giovani valdrighi");
-    //nomes.insert("giovani souza");
-    //nomes.insert("rener oliveira");
-    //nomes.serialize("second.txt");
-    nomes.diserialize("second.txt");  
-    return 0;
-}
+    file.close();
+  }
+  word.clear();
+  chrono::duration<double> dt = chrono::high_resolution_clock::now() - t0;
+  cout << "feito! \n";
+  cout << "Foram indexados " << docs << " artigos em " << dt.count() <<" segundos\n\n";
+  cout << "Serializando ...\n";
+  t0 = chrono::high_resolution_clock::now();
+  fstream file2;
+  file2.open("serial_trie");
+  teste.serialize(file2);
+  dt = chrono::high_resolution_clock::now() - t0;
+  cout << "O processo levou " << dt.count() << " segundos.\n";
+  return 0;
+} 
